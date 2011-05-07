f27ff17ff586feba4e2c9d375e4af3d4f144d678
Velvet Mirror <rfc1483@gmail.com>
2011-05-07 10:51:01 +0200

    Isolating the presentation

--- index.php
+++ index.php
@@ -5,25 +5,13 @@ $link = mysql_connect('localhost', 'root');
 mysql_select_db('blog_db', $link);
 
 $result = mysql_query('SELECT id, title FROM post', $link);
-?>
 
-<html>
-    <head>
-        <title>List of Posts</title>
-    </head>
-    <body>
-        <h1>List of Posts</h1>
-        <ul>
-            <?php while ($row = mysql_fetch_assoc($result)): ?>
-                <li>
-                    <a href="/show.php?id=<?php echo $row['id'] ?>">
-                        <?php echo $row['title'] ?>
-                    </a>
-                </li>
-            <?php endwhile; ?>
-        </ul>
-    </body>
-</html>
+$posts = array();
+while ($row = mysql_fetch_assoc($result)) {
+    $posts[] = $row;
+}
 
-<?php
 mysql_close($link);
+
+// include the HTML presentation code
+require 'templates/list.php';
