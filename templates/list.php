fcf5dec1a037c6864ea4ee061339f3e0aa1aa41c
Velvet Mirror <rfc1483@gmail.com>
2011-05-07 10:53:01 +0200

    Isolating the presentation

---
+++ templates/list.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>List of Posts</title>
+    </head>
+    <body>
+        <h1>List of Posts</h1>
+        <ul>
+            <?php foreach ($posts as $post): ?>
+                <li>
+                    <a href="/read?id=<?php echo $post['id'] ?>">
+                        <?php echo $post['title'] ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </body>
+</html>
