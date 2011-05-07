8bd0e3085a767f214125a09a7ca588ec61050691
Velvet Mirror <rfc1483@gmail.com>
2011-05-07 11:44:20 +0200

    Isolating the Layout

--- templates/list.php
+++ templates/list.php
@@ -1,17 +1,16 @@
-<html>
-    <head>
-        <title>List of Posts</title>
-    </head>
-    <body>
-        <h1>List of Posts</h1>
-        <ul>
-            <?php foreach ($posts as $post): ?>
-                <li>
-                    <a href="/read?id=<?php echo $post['id'] ?>">
-                        <?php echo $post['title'] ?>
-                    </a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </body>
-</html>
+<?php $title = 'List of Posts' ?>
+
+<?php ob_start() ?>
+<h1>List of Posts</h1>
+<ul>
+    <?php foreach ($posts as $post): ?>
+        <li>
+            <a href="/read?id=<?php echo $post['id'] ?>">
+                <?php echo $post['title'] ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+</ul>
+<?php $content = ob_get_clean() ?>
+
+<?php include 'layout.php' ?>
