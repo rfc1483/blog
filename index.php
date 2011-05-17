73a6f7845c7c86a7c571f901870696df0f2decb5
VelvetMirror <rfc1483@gmail.com>
2011-05-17 23:02:14 +0200

    Deleting libraries not used

--- index.php
+++ index.php
@@ -7,9 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 $request = Request::createFromGlobals();
 
 $uri = $request->getPathInfo();
+echo $uri;
 if ($uri == '/') {
     $response = list_action();
 } elseif ($uri == '/show' && $request->query->has('id')) {
+    echo "inside show section";
 
     $response = show_action($request->query->get('id'));
 } else {
